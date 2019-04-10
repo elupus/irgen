74629fbb498fb4514770793f8c3c7e1f61eb3652
Joakim Plate <elupus@ecce.se>
2019-04-10 19:34:49 +0200

    Add readme to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='irgen',
     version='0.1.0',
     description='A python tool for generating and converting ir formats',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     license='MIT',
     packages=find_packages('src'),
     package_dir = {'': 'src'},
